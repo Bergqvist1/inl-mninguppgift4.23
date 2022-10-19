edef7391b627a347c3f184667da82c391d9ec3fe
Admin <Admin@TEB123021>
2022-10-19 12:09:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uppgift_4_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool b = true;
+            int n = 0;
+            for (int i = 0; b == true;)
+            {
+                Console.WriteLine("Skriv in ett heltal? Vill du fortsätta? Svara j för ja eller n för nej. Skriv sedan ditt nya heltal");
+                string svar = Console.ReadLine();
+                if (svar == "j")
+                {
+                    n = int.Parse(Console.ReadLine());
+                    if (n > i)
+                    {
+                        i = n;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Detta är det största talet du skrev: " + i);
+                    b = false;
+                }
+            }
+
+        }
+    }
+}
